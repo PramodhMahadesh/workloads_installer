d41accec9f8ce8ea0f7274719be7feba69e68d48
PramodhMahadesh <pramodhx.mahadesh.k.m@intel.com>
2023-09-09 14:42:54 +0530

    Changed proxy link

--- install_sandstone.sh
+++ install_sandstone.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-export http_proxy=http://proxy-dmz.intel.com:911
-export https_proxy=http://proxy-dmz.intel.com:911
+export http_proxy=http://proxy-chain.intel.com:911
+export https_proxy=http://proxy-chain.intel.com:911
 
 
 # To remove old sandstone versions, uncomment the below four lines
