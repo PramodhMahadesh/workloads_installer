222fd917ae0dad3abd95783d750f2a2332f343ef
PramodhMahadesh <pramodhx.mahadesh.k.m@intel.com>
2023-09-22 10:14:02 +0530

    Removed intel_publish

--- BuildWorkload_needtoupdate.sh
+++ BuildWorkload_needtoupdate.sh
@@ -7,7 +7,7 @@ cd build
 
 #cmake -DREGISTRY=amr-registry.caas.intel.com/sf-cwr -DRELEASE=v23.17.6 -DPLATFORM=GNR -DBENCHMARK= -DTERRAFORM_OPTIONS="--docker --svrinfo --intel_publish --owner=mahadesx" ..
 
-cmake -DREGISTRY=amr-registry.caas.intel.com/sf-cwr -DRELEASE=v23.17.6 -DPLATFORM=GNR -DTERRAFORM_SUT='static' -DBENCHMARK= -DTERRAFORM_OPTIONS="--native --docker --intel_publish --svrinfo --owner=mahadesx" ..
+cmake -DREGISTRY=amr-registry.caas.intel.com/sf-cwr -DRELEASE=v23.17.6 -DPLATFORM=GNR -DTERRAFORM_SUT='static' -DBENCHMARK= -DTERRAFORM_OPTIONS="--native --docker --svrinfo --owner=mahadesx" ..
 
 ls -l 
 
